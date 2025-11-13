421ca54b60f16935ac3ad531bf90f3a1f1d38c4b
BooMooN <mael.brungard44320@gmail.com>
2025-11-13 16:33:59 +0100

    noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 const toggle = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav');
+const body = document.querySelector('body');
 console.log(nav, toggle);
 toggle.addEventListener('click', () => {
     const isOpen = toggle.ariaExpanded === "true";
     nav.ariaHidden = isOpen;
     toggle.ariaExpanded = !isOpen;
+    body.classList.toggle('noscroll');
 });
